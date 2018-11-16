0067659e4d5b62883981b06a7ad181ce17ed1b00
Rafał Zubala <rzubala@operatus.org>
2018-11-16 11:19:34 +0100

    spring boot demo

---
+++ src/main/java/com/zubala/crmcustomer/CrmCustomerApplication.java
@@ -0,0 +1,12 @@
+package com.zubala.crmcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmCustomerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zubala/crmcustomer/CrmCustomerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zubala.crmcustomer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrmCustomerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
