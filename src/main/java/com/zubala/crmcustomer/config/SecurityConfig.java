32ec646be4101515f0a479a784b3297899798c78
Rafał Zubala <rzubala@operatus.org>
2018-11-17 10:07:00 +0100

    security

--- src/main/java/com/zubala/crmcustomer/config/SecurityConfig.java
+++ src/main/java/com/zubala/crmcustomer/config/SecurityConfig.java
@@ -49,38 +49,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
-		if (false) {
 		http
-                .cors()
-                    .and()
-                .csrf()
-                    .disable()
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                .authorizeRequests()
-                    .antMatchers("/",
-                        "/favicon.ico",
-                        "/**/*.png",
-                        "/**/*.gif",
-                        "/**/*.svg",
-                        "/**/*.jpg",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js")
-                        .permitAll()
-                    .antMatchers("/api/auth/**")
-                        .permitAll()
-                    .antMatchers(HttpMethod.GET, "/api/customers").hasRole("USER")
-                    .anyRequest()
-                        .authenticated();
-		}
-        http.authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/api/customers").hasRole("USER")
-		.and()
-		.httpBasic()
-		.and()
-		.csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	        .cors()
+	            .and()
+	        .csrf()
+	            .disable()
+	        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	            .and()
+	        .authorizeRequests()
+	            .antMatchers("/",
+	                "/favicon.ico",
+	                "/**/*.png",
+	                "/**/*.gif",
+	                "/**/*.svg",
+	                "/**/*.jpg",
+	                "/**/*.html",
+	                "/**/*.css",
+	                "/**/*.js")
+	                .permitAll()
+	            .antMatchers("/api/auth/**").permitAll()
+	            .antMatchers(HttpMethod.GET, "/api/customers").hasAnyRole("USER", "ADMIN")
+	            .anyRequest().authenticated()
+				.and()
+				.httpBasic();
     }
 }
--- src/main/java/com/zubala/crmcustomer/rest/CustomerController.java
+++ src/main/java/com/zubala/crmcustomer/rest/CustomerController.java
@@ -2,12 +2,11 @@ package com.zubala.crmcustomer.rest;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,9 +26,9 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 
-	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
-	
 	@GetMapping("/customers")
+	//@Secured({"ROLE_USER", "ROLE_ADMIN"})
+	//@PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
 	Page<Customer> getAllCustomers(Pageable pageable) {
 		return customerService.getAllCustomers(pageable);
 	}
