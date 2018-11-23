3155d1b6653d367b65d54a52d8a28337c045f10a
Rafał Zubala <rzubala@operatus.org>
2018-11-23 09:26:42 +0100

    material-ui

--- client/react-crm/src/containers/Auth/Auth.js
+++ client/react-crm/src/containers/Auth/Auth.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 import './Auth.css';
 
@@ -35,10 +37,26 @@ class Auth extends Component {
       return (
         <div className="Auth" >
           <p>token: {this.state.token}</p>
-          <form onSubmit={this.submitHandler}>
-            <input className="AuthInput" value={this.state.username} onChange={event => this.usernameChangedHandler(event)} />
-            <input className="AuthInput" value={this.state.password} type='password' onChange={event => this.passwordChangedHandler(event)} />
-            <button className="AuthButton" >SUBMIT</button>
+          <form className="AuthForm" onSubmit={this.submitHandler}>
+            <TextField
+              id="username"
+              label="Username"
+              value={this.state.username}
+              onChange={event => this.usernameChangedHandler(event)}
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              value={this.state.password}
+              onChange={event => this.passwordChangedHandler(event)}
+              type="password"
+              autoComplete="current-password"
+              margin="normal"
+              variant="outlined"
+            />
+            <Button variant="outlined" color="primary" type="submit">SUBMIT</Button>
           </form>
         </div>
       );
