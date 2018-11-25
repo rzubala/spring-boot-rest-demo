4af0f2b560a424a8fd94991ea465bd9273cfc8b4
Rafał Zubala <rzubala@operatus.org>
2018-11-25 09:23:08 +0100

    customers table initial

--- client/react-crm/src/App.js
+++ client/react-crm/src/App.js
@@ -2,22 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import Auth from './containers/Auth/Auth';
 import Customers from './containers/Customers/Customers';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-
+    let routes = (
+      <Switch>
+        <Route path ="/" exact component={Auth} />
+        <Redirect to="/" />
+      </Switch>);
+    if (this.props.token) {
+      routes = (
         <Switch>
           <Route path ="/customers" component={Customers} />
           <Route path ="/" exact component={Auth} />
-          <Redirect to="/" />
-        </Switch>
-
+          <Redirect to="/" />          
+        </Switch>);
+    }
+    return (
+      <div className="App">
+        {routes}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    token: state.auth.token
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(App));
--- client/react-crm/src/containers/Customers/Customers.js
+++ client/react-crm/src/containers/Customers/Customers.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import * as actions from './../../store/actions/index';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import './Customers.css';
 
 class Customers extends Component {
 
@@ -14,15 +22,43 @@ class Customers extends Component {
   }
 
   render() {
-    let redirect = null;
-    if (!this.props.token) {
-      redirect = <Redirect to ="/" />
+    let rows = null;
+    if (this.props.customers) {
+      rows = this.props.customers.map(row => {
+        return (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">{row.firstName}</TableCell>
+            <TableCell>{row.lastName}</TableCell>
+            <TableCell>{row.email}</TableCell>
+            <TableCell>{row.createdAt}</TableCell>
+            <TableCell>{row.updatedAt}</TableCell>
+          </TableRow>
+        );
+      });
     }
+
+    const headers = (
+      <TableRow>
+        <TableCell>First name</TableCell>
+        <TableCell>Last name</TableCell>
+        <TableCell>Email</TableCell>
+        <TableCell>Created at</TableCell>
+        <TableCell>Updated at</TableCell>
+      </TableRow>
+    );
+
     return (
-      <div>
-        {redirect}
+      <Paper className="Customers">
         <h1>CUSTOMERS</h1>
-      </div>
+        <Table>
+          <TableHead>
+            {headers}
+          </TableHead>
+          <TableBody>
+            {rows}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
