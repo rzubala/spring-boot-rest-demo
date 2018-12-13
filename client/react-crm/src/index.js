a7b72f76860734a3c355501b2cc7eb042ceabeba
Rafał Zubala <rzubala@operatus.org>
2018-12-13 22:12:33 +0100

    development mode - redux plugin fix

--- client/react-crm/src/index.js
+++ client/react-crm/src/index.js
@@ -10,7 +10,7 @@ import thunk from 'redux-thunk';
 import authReducer from './store/reducers/auth';
 import customersReducer from './store/reducers/customers';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) : null || compose;
 
 const rootReducer = combineReducers({
     auth: authReducer,
