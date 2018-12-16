6c8ca31382f71b1fa8d44548f38800c7b4aeca54
Rafał Zubala <rzubala@operatus.org>
2018-12-16 18:13:32 +0100

    updated Profile container

--- client/react-crm/src/containers/Profile/Profile.js
+++ client/react-crm/src/containers/Profile/Profile.js
@@ -4,19 +4,39 @@ import { Redirect } from 'react-router';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 import CancelIcon from '@material-ui/icons/Cancel';
+import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
 
 import { CUSTOMERS_PATH } from '../Customers/Customers';
 
 import './Profile.css';
 
+const LOGIN = 'login';
+const PASSWORD = 'password';
+const PASSWORD2 = 'password2';
+const EMAIL = 'email';
+const PHONE = 'phone';
+
 class Profile extends Component {
 
     state = {
-        redirectTo: null
+        redirectTo: null,
+        username: null,
+        password: null,
+        password2: null,
+        email: null,
+        phone: null
     }
 
     submitHandler() {
+        event.preventDefault();
+    }
 
+    onFieldChange = (event, type) => {
+        this.setState({
+            ...this.state,
+            [type]: event.target.value
+        });
     }
 
     onCancel = () => {
@@ -34,7 +54,61 @@ class Profile extends Component {
             <div className="Profile">
                 <h1>Profile</h1>
                 {redirect}
-                <form onSubmit={this.submitHandler}>                   
+                <form className="ProfileForm" onSubmit={this.submitHandler}>                   
+
+                    <TextField
+                        required
+                        id="standard-password-input"
+                        label="Login"
+                        className="ProfileInput"
+                        value={this.state.login}
+                        onChange={(event) => this.onFieldChange(event, LOGIN)}
+                        autoComplete="login"
+                        margin="normal" />
+
+                    <TextField
+                        required
+                        id="standard-password-input"
+                        label="Password"
+                        className="ProfileInput"
+                        type="password"
+                        value={this.state.password}
+                        onChange={(event) => this.onFieldChange(event, PASSWORD)}
+                        autoComplete="current-password"
+                        margin="normal" />
+
+                    <TextField
+                        required
+                        id="standard-password-input"
+                        label="Repeat password"
+                        className="ProfileInput"
+                        type="password"
+                        value={this.state.password}
+                        onChange={(event) => this.onFieldChange(event, PASSWORD)}
+                        autoComplete="current-password"
+                        margin="normal" />
+
+                    <TextField
+                        required
+                        id="standard-password-input"
+                        label="E-mail"
+                        className="ProfileInput"
+                        type="email"
+                        value={this.state.email}
+                        onChange={(event) => this.onFieldChange(event, EMAIL)}
+                        autoComplete="email"
+                        margin="normal" />
+
+                    <TextField
+                        id="standard-password-input"
+                        label="Phone"
+                        className="ProfileInput"
+                        type="phone"
+                        value={this.state.phone}
+                        onChange={(event) => this.onFieldChange(event, PHONE)}
+                        autoComplete="phone"
+                        margin="normal" />
+                        
                     <div className="ProfileButtons">
                         <div className="ProfileButton" >
                             <Button variant="contained" color="primary" style={{width: '100%'}} type="submit">
